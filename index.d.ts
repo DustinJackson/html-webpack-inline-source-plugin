c4777196f82bc10215f845923cbeac0ce98980f4
Stafford Brunk <wingrunr21@gmail.com>
2019-01-01 09:51:38 -0700

    Add constructor definition to TS types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
 import { Plugin } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export = HtmlWebpackInlineSourcePlugin;
-declare class HtmlWebpackInlineSourcePlugin extends Plugin { }
+declare class HtmlWebpackInlineSourcePlugin extends Plugin {
+  constructor(htmlWebpackPlugin: HtmlWebpackPlugin)
+}
 declare namespace HtmlWebpackInlineSourcePlugin { }
